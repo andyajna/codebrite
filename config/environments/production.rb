787d175182e895638895e9f22179ac8384a134bd
Andrew Kemp <andrewkemp70@gmail.com>
2013-11-08 14:54:09 +1100

    initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,6 @@ Codebrite::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => "herokuapp.com" }
 end
